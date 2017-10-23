a623988e31a34c07a4e32d589e3a9a5ca9187c03
dionSong <martinezdaniel082014@gmail.com>
2017-10-23 14:35:41 -0500

    styled shit a little bit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,12 @@ import { AppComponent } from './app.component';
 import { ListComponent } from './components/list/list.component';
 import { ChirpFormComponent } from './components/chirp-form/chirp-form.component';
 
+import {MatButtonModule, MatCheckboxModule, } from '@angular/material';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +18,19 @@ import { ChirpFormComponent } from './components/chirp-form/chirp-form.component
     ChirpFormComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
